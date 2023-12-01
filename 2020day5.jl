42c85a2fcaff725b57e72ddef3cbfbe551ab4c39
andershayd <haydensolo@gmail.com>
2023-12-01 21:41:14 +1300

    Day 1

--- 2020day5.jl
+++ 2020day5.jl
@@ -1,3 +1,5 @@
+include("utils.jl")
+
 input = readinput()
 
 testlin = "FBFBBFFRLR"
---
+++ day1.jl
@@ -0,0 +1,33 @@
+input = readinput()
+
+function getval(x) 
+    s = replace(x, r"[a-zA-Z]"=>"")
+    parseint("$(s[1])$(s[end])")
+end
+
+all = getval.(input)
+println(sum(all))
+
+function getval2(txt)
+    pat = r"one|two|three|four|five|six|seven|eight|nine|[1234567890]"
+    words = [ match.match for match in eachmatch(pat, txt; overlap=true)]
+    parser = Dict(
+        "one"=>"1",
+        "two"=>"2",
+        "three"=>"3",
+        "four"=>"4",
+        "five"=>"5",
+        "six"=>"6",
+        "seven"=>"7",
+        "eight"=>"8",
+        "nine"=>"9",
+    )
+    word1 = words[1]
+    word2 = words[end]
+    occursin(word1, "123456789") || (word1 = parser[word1])
+    occursin(word2, "123456789") || (word2 = parser[word2])
+    parseint("$(word1)$(word2)")
+end
+
+all = getval2.(input)
+println(sum(all))
