3e6bd6eeecc62721b218a434d7a55b8cea86bb6a
andershayd <haydensolo@gmail.com>
2023-12-12 17:58:12 +1300

    Utils matrix read

--- utils.jl
+++ utils.jl
@@ -7,3 +7,14 @@ parseint(ss) = parseint.(ss)
 parsefloat(ss) = parsefloat.(ss)
 
 maketypes(types...) = foreach(t -> eval(:(struct $(Symbol(t)) end)), types)
+
+function readmatrix(test::Bool=false)
+    input = String.(hcat(split.(readinput(test), "")...))
+    flip = similar(input, String, size(input)[2], size(input)[1])
+    for i in 1:size(input)[1]
+        for j in 1:size(input)[2]
+            flip[j, i] = input[i, j]
+        end
+    end
+    flip
+end
